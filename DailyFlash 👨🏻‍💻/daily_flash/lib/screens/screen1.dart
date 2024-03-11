b97dfb29f36ab47ac254fa873067a258e98bc51b
AbhishekASLK <bhoslea925@gmail.com>
2024-03-11 14:47:13 +0530

    trash

---
+++ DailyFlash 👨🏻‍💻/daily_flash/lib/screens/screen1.dart
@@ -0,0 +1,35 @@
+import 'package:daily_flash/screens/screen2.dart';
+import 'package:flutter/material.dart';
+
+class Screen1 extends StatelessWidget {
+  const Screen1({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: const Icon(Icons.menu),
+        title: const Text('Instagram'),
+        centerTitle: true,
+        actions: const [
+          Icon(Icons.camera),
+          SizedBox(
+            width: 12,
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const Screen2();
+          }));
+        },
+        child: const Icon(
+          Icons.skip_next_outlined,
+          size: 30,
+        ),
+      ),
+    );
+  }
+}
