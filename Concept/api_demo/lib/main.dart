8f1c1f32f5d38c372ca2d40ff9063afa4e4c5206
Abhishek Bhosle <108826846+AbhishekASLK@users.noreply.github.com>
2024-03-29 12:06:36 +0530

    changed the way of writting code

--- Concept/api_demo/lib/main.dart
+++ Concept/api_demo/lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:api_demo/api_demo.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() {
+dynamic data;
+void main() async {
+  await getData();
   runApp(const MyApp());
+}
+
+Future getData() async {
+  var response = await http.get(
+    Uri.parse('https://jsonplaceholder.typicode.com/todos'),
+  );
+  data = jsonDecode(response.body);
 }
 
 class MyApp extends StatelessWidget {
